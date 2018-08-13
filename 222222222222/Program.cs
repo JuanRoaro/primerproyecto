3602ef262dd77901098f4a5088312534baeb8846
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2018-08-13 08:26:59 -0700

    Agregar archivos de proyecto.

---
+++ 222222222222/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona persona1 = new Persona(25, "José");
+
+            Console.WriteLine(persona1.nombre + " tiene " + persona1.edad + " años.");
+
+            Console.ReadLine();
+        }
+    }
+}
